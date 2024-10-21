8a0999079b5fb3cdb0cf945a97beaad5da7c3d19
AnmolGautam <anmolgautam2428@gmail.com>
2024-10-21 23:38:12 +0530

    ch11-ch15 : CRUD part 1 without database

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from fastapi import FastAPI
 from fastapi import Body
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional,List,Dict,Any
+import json
+from random import randrange
 
 app = FastAPI()
 
+#------------CRUD------------#
+#standard convention for api -- use plurals, like posts, users
+#use an identifier in db to get, delete, patch or put a post
+#put ---> pass all the information for updating few or all the info
+#patch ----> send only the necessary field that is to be updated ---- will not be using this now
 
-#lets define the schema that we want from user ---- title - str, content - str
+
+#lets start saving posts in memory using a global var
+my_posts = [] 
+my_posts.append({"title":"Favourite Pizza","content":"Maragarittaaaaaaa","id":1})
+my_posts.append({"title":"Favourite Movie","content":"Matrixxxxx","id":2})
+
 
 class Post(BaseModel):
-    title:str  # field and type
+    title:str  
     content:str
-    published:bool = True #if user wants it to be published it or not default value is True
-    rating:Optional[int] = None #rating from user but fully optional
+    published:bool = True 
+    rating:Optional[int] = None 
 
 
 @app.get("/")
@@ -23,26 +35,14 @@ def home():
 
 @app.get("/posts")
 def get_posts():
-    return {
-        "data":"This is your new post"
-    }
+    return {"data":my_posts}
 
-
-
-@app.post("/createposts")
+@app.post("/posts") #update the naming for createposts to posts
 def create_posts(payload:Post): 
-    # body is good but issue is that user can send anything in request body or  payload and we are not able to validate it therefore we need to force client to send data in a schema that WE EXPECT!! Solution is -- PYDANTIC
-
-    #pydantic will check if the payload has title and content or not -- and both are to be strings therefore it will do complete validation of data send by frontend
-    print(payload.title) #we can extract data easily now
-    print(payload.rating)
-
-    print(payload.model_dump()) # each pydantic object has a inbuilt function called dict() but its deprecated now use model_dump()
-    # return {
-    #     "data":"new post"
-    # }
-    #now we can return payload
-    return payload.model_dump()
+    post_dict = payload.model_dump()
+    post_dict['id'] = randrange(0,10000000)
+    my_posts.append(post_dict)
+    return {"data":payload.model_dump()} #generally when post request is created it should respond with the data
 
 
 
