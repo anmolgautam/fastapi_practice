da6c340cba17787a7589fd46bcf933127c94b125
AnmolGautam <anmolgautam2428@gmail.com>
2024-10-22 11:02:57 +0530

    CRUD Part 3 - delete and update

--- main.py
+++ main.py
@@ -10,14 +10,6 @@ from fastapi import HTTPException
 
 app = FastAPI()
 
-#------------CRUD------------#
-#standard convention for api -- use plurals, like posts, users
-#use an identifier in db to get, delete, patch or put a post
-#put ---> pass all the information for updating few or all the info
-#patch ----> send only the necessary field that is to be updated ---- will not be using this now
-
-
-#lets start saving posts in memory using a global var
 my_posts = [] 
 my_posts.append({"title":"Favourite Pizza","content":"Maragarittaaaaaaa","id":1})
 my_posts.append({"title":"Favourite Movie","content":"Matrixxxxx","id":2})
@@ -31,8 +23,13 @@ class Post(BaseModel):
 
 def find_post(id):
     for post in my_posts:
-        if post["id"]==id: #can remove int(id) here to id as we are forcing validation at request level
+        if post["id"]==id: 
             return post
+        
+def find_post_index(id):
+    for idx,p in enumerate(my_posts):
+        if p["id"] == id:
+            return idx
 
 @app.get("/")
 def home():
@@ -44,29 +41,40 @@ def home():
 def get_posts():
     return {"data":my_posts}
 
-@app.post("/posts",status_code=status.HTTP_201_CREATED) #update the naming for createposts to posts
-def create_posts(payload:Post): #set up a status code here as well based on mdn docs if an entity is created send a response 201 in router
+@app.post("/posts",status_code=status.HTTP_201_CREATED) 
+def create_posts(payload:Post): 
     post_dict = payload.model_dump()
     post_dict['id'] = randrange(0,10000000)
     my_posts.append(post_dict)
-    return {"data":payload.model_dump()} #generally when post request is created it should respond with the data
+    return {"data":payload.model_dump()} 
 
 
-@app.get("/posts/{id}") #id here is path parameter, fastapi automatically parses the id so we can pass it to the function directly
-def get_post(id:int): #we want id to be integer thereby forcing validation
+@app.get("/posts/{id}") 
+def get_post(id:int): 
     print(id)
-    # for post in my_posts:
-    #     if post["id"]==int(id):
-    #         return {"post_detail":post}
-    # since finding a post based on id can be separated out as a separate logic so lets create a method/function for it and call it here
-
-    #if suppose a id doesnt exist instead of returning not found return valid HTTP exceptions, so frontend gets proper info - 404 error - item not found or resource doesn't exist 
     post = find_post(id)
     if not post:
-        #response.status_code = status.HTTP_404_NOT_FOUND #we can use status object from fastapi to set it up instead of manually setting response.status_code = 404
-        #return {"post_details":f"Post with {id} not found"} # but its not fol proof raise a HTTP exception
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id : {id} not found")
-    
     return{"post_details":post} 
 
-#IMP ------ if we create anopther route like "/posts/latest" to get the latest post then it will go to /posts/{id} and will give validation error therefore ordering of API routes are important.
+
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id:int):
+    index = find_post_index(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post not found with id : {id}")
+    my_posts.pop(index)
+    #return {"message":"Post deleted successfully"} #when deleting something do not return anything except the status code
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id:int,post:Post):
+    print(post)
+    index = find_post_index(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id : {id} not found")
+    post_dict = post.model_dump()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    return {"data":post_dict}
